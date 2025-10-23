f8acb9956dde933a63d765d9ef0fa2bc69c2824c
Filippo29 <filodev06@gmail.com>
2025-10-23 20:51:38 +0200

    added rk4 integration

--- Include/Imu.h
+++ Include/Imu.h
@@ -39,6 +39,7 @@ class Imu : public Sensor {
         std::string csvHeader = "q0,qx,qy,qz,wx,wy,wz";
         capture = new Logger(std::to_string(robotNumber), csvHeader);
         captureNoise = new Logger(std::to_string(robotNumber) + "_noise", csvHeader);
+        captureNoise2 = new Logger(std::to_string(robotNumber) + "_noise2", csvHeader);
 
         gyroBias.setZero();
 
@@ -50,23 +51,38 @@ class Imu : public Sensor {
         normalDist = std::normal_distribution<double>(0., gyroNoiseStd);
     }
 
+    //double theta;
     void update() {
         orientation = Eigen::Map<const Eigen::Vector4d>(mujData->sensordata + orientationAdr, orientationDim);
+        if (first) {
+            orientationQuat = Eigen::Quaterniond(orientation[0], orientation[1], orientation[2], orientation[3]);
+            orientationQuatNoisy = Eigen::Quaterniond(orientation[0], orientation[1], orientation[2], orientation[3]);
+            first = false;
+        } else {
+            integrateOrientationRK4(orientationQuat, angularVelocity, 0.01);
+            integrateOrientationRK4(orientationQuatNoisy, angularVelocityNoisy, 0.01);
+        }
         angularVelocity = Eigen::Map<const Eigen::Vector3d>(mujData->sensordata + gyroAdr, gyroDim);
+        addNoise(angularVelocity, angularVelocityNoisy);
         
         if(capture) {
             capture->log(orientation[0], orientation[1], orientation[2], orientation[3], 
                     angularVelocity[0], angularVelocity[1], angularVelocity[2]);
 
-            addNoise(orientation, angularVelocity);
-            captureNoise->log(orientation[0], orientation[1], orientation[2], orientation[3], 
-                    angularVelocity[0], angularVelocity[1], angularVelocity[2]);
+            captureNoise->log(orientationQuat.w(), orientationQuat.x(), orientationQuat.y(), orientationQuat.z(), 
+                    angularVelocityNoisy[0], angularVelocityNoisy[1], angularVelocityNoisy[2]);
+
+            captureNoise2->log(orientationQuatNoisy.w(), orientationQuatNoisy.x(), orientationQuatNoisy.y(), orientationQuatNoisy.z(),
+                    "", "", "");
         }
     };
 
     Eigen::Vector4d orientation;      // [q0, qx, qy, qz] : orientation of the Imu wrt the world frame
+    bool first = true;
+    Eigen::Quaterniond orientationQuat;
+    Eigen::Quaterniond orientationQuatNoisy;
     Eigen::Vector3d angularVelocity;  // [wx, wy, wz] : angular velocity expressed in the local frame of the
-                                      // Imu
+    Eigen::Vector3d angularVelocityNoisy;
 
    private:
     mjModel* mujModel;
@@ -79,6 +95,7 @@ class Imu : public Sensor {
 
     Logger* capture;
     Logger* captureNoise;
+    Logger* captureNoise2;
 
     double gyroNoiseStd;       // rad/s discrete
     double gyroBiasWalkStd;    // rad/s discrete (random walk step)
@@ -87,7 +104,7 @@ class Imu : public Sensor {
     std::default_random_engine gen;
     std::normal_distribution<double> normalDist;
 
-    void addNoise(Eigen::Vector4d& orientation, Eigen::Vector3d& angularVelocity)
+    void addNoise(const Eigen::Vector3d& angularVelocity, Eigen::Vector3d& angularVelocityNoisy)
     {
         // update bias for random walk component
         for (int i = 0; i < 3; ++i)
@@ -95,7 +112,47 @@ class Imu : public Sensor {
 
         // add normal noise + random wal bias
         for (int i = 0; i < 3; ++i)
-            angularVelocity(i) += gyroBias(i) + gyroNoiseStd * normalDist(gen);
+            angularVelocityNoisy(i) = angularVelocity(i) + gyroBias(i) + gyroNoiseStd * normalDist(gen);
+    }
+
+    Eigen::Quaterniond quatDerivative(const Eigen::Quaterniond& q, const Eigen::Vector3d& omega) {
+        Eigen::Quaterniond omegaQuat(0.0, omega.x(), omega.y(), omega.z());
+        Eigen::Quaterniond dq = q * omegaQuat;
+        dq.coeffs() *= 0.5;
+        return dq;
+    }
+
+    Eigen::Quaterniond quatAddScaled(const Eigen::Quaterniond& q, const Eigen::Quaterniond& k, double a) {
+        Eigen::Quaterniond result = q;
+        result.coeffs() += a * k.coeffs();
+        return result;
+    }
+
+    void integrateOrientationRK4(
+        Eigen::Quaterniond& q,
+        const Eigen::Vector3d& omega,
+        double dt)
+    {
+        Eigen::Quaterniond k1 = quatDerivative(q, omega);
+        Eigen::Quaterniond k2 = quatDerivative(quatAddScaled(q, k1, 0.5 * dt), omega);
+        Eigen::Quaterniond k3 = quatDerivative(quatAddScaled(q, k2, 0.5 * dt), omega);
+        Eigen::Quaterniond k4 = quatDerivative(quatAddScaled(q, k3, dt), omega);
+
+        q.coeffs() += (dt / 6.0) * (k1.coeffs() + 2.0 * k2.coeffs() + 2.0 * k3.coeffs() + k4.coeffs());
+        q.normalize();
+    }
+
+    inline void integrateAngle(Eigen::Quaterniond& orientation,
+        const Eigen::Vector3d& angularVelocity,
+        double dt)
+    {
+        Eigen::Quaterniond w(0., angularVelocity[0], angularVelocity[1], angularVelocity[2]);
+
+        Eigen::Quaterniond qdot = orientation * w;
+        qdot.coeffs() *= 0.5 * dt;
+
+        orientation.coeffs() += qdot.coeffs();
+        orientation.normalize();
     }
 };
 }  // namespace spqr
