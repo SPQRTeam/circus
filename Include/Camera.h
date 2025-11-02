289fb2f0f3bdc880c1c5216f804bb668f26a2bf0
DaniAffCH <danieleaffinita2000@gmail.com>
2025-11-02 16:06:32 +0100

    camera not found check

--- Include/Camera.h
+++ Include/Camera.h
@@ -10,11 +10,12 @@ class Camera : public Sensor {
     Camera(MujocoContext* mujContext, const char* cameraName) : mujContext(mujContext) {
         cam.type = mjCAMERA_FIXED;
         cam.fixedcamid = mj_name2id(mujContext->model, mjOBJ_CAMERA, cameraName);
+
+        if (cam.fixedcamid < 0)
+            throw std::runtime_error(std::string("Camera not found: ") + cameraName);
     }
 
     void doUpdate() override {
-        mjv_updateScene(mujContext->model, mujContext->data, &mujContext->opt, nullptr, &cam, mjCAT_ALL,
-                        &mujContext->scene);
     };
 
     msgpack::object doSerialize(msgpack::zone& z) override {
@@ -23,7 +24,7 @@ class Camera : public Sensor {
 
    private:
     MujocoContext* mujContext;
-
+    std::vector<uint8_t> image;
     mjvCamera cam{};
 };
 }  // namespace spqr
