0a97b6d3fe9a6979dd562f2b9343be5c65754720
Valerio Spagnoli <spagnoli.valerio.00@gmail.com>
2026-01-10 13:10:40 +0100

    light

--- src/SceneParser.cpp
+++ src/SceneParser.cpp
@@ -166,16 +166,13 @@ string SceneParser::buildMuJoCoXml() {
     xml_node actuator = mujoco.append_child("actuator");
     xml_node sensor = mujoco.append_child("sensor");
 
-    // xml_node light = worldbody.append_child("light");
-    // light.append_attribute("ambient") = "0.5 0.5 0.5";
-    // light.append_attribute("diffuse") = "0.7 0.7 0.7";
-    // light.append_attribute("specular") = "0.3 0.3 0.3";
-    // light.append_attribute("pos") = "0 0 100";
-    // light.append_attribute("dir") = "0 0 -1";
-    // light.append_attribute("castshadow") = "false";
-    // light.append_attribute("attenuation") = "1 0 0";
-    // light.append_attribute("cutoff") = "60";
-    // light.append_attribute("exponent") = "10";   
+    xml_node light = worldbody.append_child("light");
+    light.append_attribute("ambient") = "0.4 0.4 0.4";
+    light.append_attribute("diffuse") = "0.5 0.5 0.5";
+    light.append_attribute("specular") = "0.5 0.5 0.5";
+    light.append_attribute("pos") = "0 0 100";
+    light.append_attribute("dir") = "0 0 -1";
+    light.append_attribute("castshadow") = "false";
 
     for (const shared_ptr<Team>& team : scene.teams) {
         for (const shared_ptr<Robot>& robot : team->robots) {
