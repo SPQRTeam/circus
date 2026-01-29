1d0dc7b301f643c5dfd65044d8bbc6c9acf0a591
neverorfrog <97flavio.maiorana@gmail.com>
2026-01-29 23:12:55 +0100

    installed config files into docker container

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -5,8 +5,8 @@ export LD_LIBRARY_PATH=/app/booster_motion/lib:/app/booster_motion/lib-usr-local
 export FASTRTPS_DEFAULT_PROFILES_FILE=/app/booster_motion/fastdds_profile.xml
 
 # Export environment variables for MAXIMUS framework
-export SPQR_CONFIG_ROOT=/app/maximus/src/app/config
-export SPQR_BEHAVIOR_TREE_PATH=/app/maximus/src/app/behaviors/trees
+export SPQR_CONFIG_ROOT=/app/maximus/config
+export SPQR_BEHAVIOR_TREE_PATH=/app/maximus/behaviors
 
 # Start supervisord to manage processes
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/booster.conf
