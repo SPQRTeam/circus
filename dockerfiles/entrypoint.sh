fe3512ee123f107fb1919f4c3e34b321f0f2f600
Francesco Petri <francesco.petri@uniroma1.it>
2026-03-04 18:16:00 +0100

    Add symlink to tensorrt

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -8,5 +8,9 @@ export FASTRTPS_DEFAULT_PROFILES_FILE=/app/booster_motion/fastdds_profile.xml
 export SPQR_CONFIG_ROOT=/app/maximus/config
 export SPQR_BEHAVIOR_TREE_PATH=/app/maximus/behaviors
 
+source /app/maximus/tools/vision/scripts/detect-tensorrt.sh
+TENSORRT_LINKNAME=$(echo $TENSORRT_DIR | sed 's:[^/]*$:TensorRT:')
+ln -s $TENSORRT_DIR $TENSORRT_LINKNAME
+
 # Start supervisord to manage processes
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/booster.conf
