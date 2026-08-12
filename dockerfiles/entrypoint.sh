4498fa6582d6551431a4f2f30cf9b299d345b139
michelebri <michelebrienza1997@gmail.com>
2026-08-12 20:30:05 +0800

    fix(dds): configure FastDDS variables for SDK 1.7

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -3,6 +3,8 @@
 # Export dynamic libraries and fastdds profile for booster_motion execution
 export LD_LIBRARY_PATH=/app/booster_motion/lib:/app/booster_motion/lib-usr-local:/app/booster_motion/lib-x86_64-linux-gnu:$LD_LIBRARY_PATH
 export FASTRTPS_DEFAULT_PROFILES_FILE=/app/booster_motion/fastdds_profile.xml
+export FASTDDS_DEFAULT_PROFILES_FILE=/app/booster_motion/fastdds_profile.xml
+export BOOSTER_FASTRTPS_DEFAULT_PROFILES_FILE=/app/booster_motion/fastdds_profile.xml
 
 # Export environment variables for MAXIMUS framework
 export SPQR_CONFIG_ROOT=/app/maximus/config
